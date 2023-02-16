eab32545ac109a70aae1a992460fc737937a6894
Aatif Ali <aatifwarraich10@gmail.com>
2023-02-17 00:17:11 +0500

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,19 +2,10 @@ import LeftSideBar from "./component/leftside/LeftSideBar";
 import Feed from "./component/feed/Post";
 import RightSidebar from "./component/rightsidebar/RightSidebar";
 import Header from "./component/header/Header";
-// import LoginForm from "./pages/LoginForm";
-// import { Routes, Route } from "react-router";
-// import RegisterForm from "./pages/RegisterForm";
-// import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <Routes>
-        <LoginForm />
-        <Route path="/RegisterForm" element={<RegisterForm />} />
-      </Routes>
-      <Link to="/"> */}
       <div>
         <Header />
         <div
@@ -26,7 +17,6 @@ function App() {
           <RightSidebar />
         </div>
       </div>
-      {/* </Link> */}
     </>
   );
 }
